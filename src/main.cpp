8bb9fb1fbf9a0a55231f102f6be43ed816101900
caeljk <caeljacobkennedy@hotmail.com>
2025-03-15 15:20:00 +1000

    Define initial ANSI clear and reset codes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #  define NOMINMAX
 #  include <windows.h>
 #endif
+namespace ansi {
+    constexpr const char* RST  = "\033[0m";
+    constexpr const char* CLR  = "\033[2J\033[H";
+}
 int main() {
-    std::cout << "initialized
-";
+    std::cout << ansi::CLR << "Started\n";
     return 0;
 }
