d28158b76364a4cc8988de135e185293b33c486f
caeljk <caeljacobkennedy@hotmail.com>
2025-03-28 14:45:00 +1000

    Implement cursor reposition ANSI escape helper

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
 #ifdef _WIN32
 #  define NOMINMAX
 #  include <windows.h>
 #endif
 namespace ansi {
+    inline std::string mv(int r, int c) {
+        return "\033[" + std::to_string(r) + ";" + std::to_string(c) + "H";
+    }
     constexpr const char* RST  = "\033[0m";
     constexpr const char* CLR  = "\033[2J\033[H";
 }
 int main() {
-    std::cout << ansi::CLR << "Started\n";
+    std::cout << ansi::CLR << ansi::mv(1, 1) << "Ready" << ansi::RST << std::endl;
     return 0;
 }
