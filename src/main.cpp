f8affd1dbcdeba6ac89f879e7e833d5de9538bc2
caeljk <caeljacobkennedy@hotmail.com>
2025-05-05 11:45:00 +1000

    Draft term initialization structures

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 #ifdef _WIN32
 #  define NOMINMAX
 #  include <windows.h>
+#else
+#  include <termios.h>
+#  include <unistd.h>
 #endif
-namespace ansi {
-    constexpr const char* EL   = "\033[K";
-    inline std::string rgb(int r, int g, int b) {
-        return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + ";m";
+
+namespace term {
+#ifdef _WIN32
+    void init() {
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        DWORD mode = 0;
+        GetConsoleMode(hOut, &mode);
+        SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
     }
-    inline std::string mv(int r, int c) {
-        return "\033[" + std::to_string(r) + ";" + std::to_string(c) + "H";
-    }
-    constexpr const char* RST  = "\033[0m";
-    constexpr const char* CLR  = "\033[2J\033[H";
-    constexpr const char* HIDE = "\033[?25l";
-    constexpr const char* SHOW = "\033[?25h";
+    void restore() {}
+#else
+    void init() {}
+    void restore() {}
+#endif
 }
 int main() {
-    std::cout << ansi::CLR << ansi::mv(1, 1) << "Ready" << ansi::RST << std::endl;
     return 0;
 }
