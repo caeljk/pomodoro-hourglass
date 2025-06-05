37d6ac5942610a67bb25c99129e503001b73ab4a
caeljk <caeljacobkennedy@hotmail.com>
2025-06-05 12:00:00 +1000

    Implement Unix termios configuration

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,24 @@ namespace term {
         GetConsoleMode(hOut, &mode);
         SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
     }
-    void restore() {}
+    void restore() {
+        tcsetattr(STDIN_FILENO, TCSANOW, &saved);
+    }
     bool kbhit() { return _kbhit() != 0; }
     int getch() { return _getch(); }
 #else
-    void init() {}
-    void restore() {}
+    static termios saved;
+    void init() {
+        tcgetattr(STDIN_FILENO, &saved);
+        termios t = saved;
+        t.c_lflag &= ~(ECHO | ICANON);
+        t.c_cc[VMIN]  = 0;
+        t.c_cc[VTIME] = 0;
+        tcsetattr(STDIN_FILENO, TCSANOW, &t);
+    }
+    void restore() {
+        tcsetattr(STDIN_FILENO, TCSANOW, &saved);
+    }
     bool kbhit() { return _kbhit() != 0; }
     int getch() { return _getch(); }
 #endif
